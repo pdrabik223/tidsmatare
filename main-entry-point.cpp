1ce2b5c24dba73c1658a7c1f84d12c51d2bc541b
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-09-30 23:07:07 +0200

    function for testing purposes

--- main-entry-point.cpp
+++ main-entry-point.cpp
@@ -2,8 +2,16 @@
 // Created by piotr233 on 30.09.2021.
 //
 
+#include <iostream>
+#include <thread>
+void OneSecondFunction();
+
 int main() {
+
+  OneSecondFunction();
 
   return 0;
 
 }
+
+void OneSecondFunction(){std::this_thread::sleep_for(std::chrono::milliseconds(30));}
