a34f9b0beebd65e907579b7d0296aae92142c3f2
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-10-01 11:39:14 +0200

    delay minimisation

--- stopper/console_stopper.cpp
+++ stopper/console_stopper.cpp
@@ -7,18 +7,20 @@ void ConsoleStopper::StartClock() {
   if (clock_state_ == ClockState::STOPPED)
     off_time_ = std::chrono::duration_cast<std::chrono::milliseconds>(
         std::chrono::high_resolution_clock::now() - clock_stop_point_);
-  clock_start_point_ = std::chrono::high_resolution_clock::now();
   clock_state_ = ClockState::RUNNING;
+
+  clock_start_point_ = std::chrono::high_resolution_clock::now();
 }
 void ConsoleStopper::StopClock() {
   clock_stop_point_ = std::chrono::high_resolution_clock::now();
   clock_state_ = ClockState::STOPPED;
 }
 void ConsoleStopper::Measure() {
+  auto now = std::chrono::high_resolution_clock::now();
+
   if (clock_state_ == ClockState::RUNNING)
     timings_.emplace_back(std::chrono::duration_cast<std::chrono::milliseconds>(
-        std::chrono::high_resolution_clock::now() - clock_start_point_ -
-        off_time_));
+        now - clock_start_point_ - off_time_));
 }
 void ConsoleStopper::EndClock() {
   Measure();
